9eb730d5dec96177053f9a0eded65b8dd42ce060
sebpalluel <sebpalluel@free.fr>
2017-09-07 21:05:10 +0200

    factorization of map draw + player in red

--- includes/wolf3d_struct.h
+++ includes/wolf3d_struct.h
@@ -6,7 +6,7 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/12 20:32:54 by psebasti          #+#    #+#             */
-/*   Updated: 2017/09/07 20:12:25 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/09/07 20:57:01 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ typedef struct		s_pathmaker
 typedef struct		s_player
 {
 	t_pix			*dir;
-	t_pix			*pos;
+	t_pix			pos;
 	t_pix			*plane;
 	size_t			randpos;
 }					t_player;
--- srcs/player.c
+++ srcs/player.c
@@ -6,15 +6,25 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/07 20:06:31 by psebasti          #+#    #+#             */
-/*   Updated: 2017/09/07 20:21:09 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/09/07 20:59:49 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/wolf3d.h"
 
-void	ft_pos_player(t_setup *setup)
+void		ft_pos_player(t_setup *setup)
 {
-	if (setup)
-		return ;
+	t_pix	pos;
 
+	ft_memset(&pos, 0, sizeof(t_pix));
+	if (!PLAY->randpos)
+	{
+		while (MAP->map[pos.y][pos.x] != PATH_)
+		{
+			pos.x = ft_random(1, M_HEIGHT - 1, 1);
+			pos.y = ft_random(1, M_WIDTH - 1, 1);
+		}
+		ft_pixcpy(&pos, &PLAY->pos);
+		PLAY->randpos = 1;
+	}
 }
