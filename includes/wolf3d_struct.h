7e4ce0667d3b514cbb3c27bfd22167b9254b4516
psebasti <psebasti@42.fr>
2017-10-05 13:05:11 +0200

    remove mid[2] from struct

--- includes/wolf3d_struct.h
+++ includes/wolf3d_struct.h
@@ -6,7 +6,7 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/12 20:32:54 by psebasti          #+#    #+#             */
-/*   Updated: 2017/10/04 15:21:45 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/10/05 13:04:42 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,6 @@ typedef struct		s_map
 	size_t			skybox;
 	int				draw_tick;
 	int				mapsize[2];
-	float			mid[2];
 	t_color			north;
 	t_color			south;
 	t_color			east;
